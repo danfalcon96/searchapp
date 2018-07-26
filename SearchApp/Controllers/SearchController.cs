52d223a16c95f76fd4d7f4d27592e5bc8c97f9af
Dan Falcon <dan_1196@yahoo.com>
2018-07-26 21:17:06 +0800

    Init searching

---
+++ SearchApp/Controllers/SearchController.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using SearchApp.Search;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SearchApp.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Index(string name)
+        {
+            Searcher searcher = new Searcher();
+            JObject result = await searcher.SearchAsync(name);
+            if (!string.IsNullOrEmpty(name))
+            {
+                return Json(JsonConvert.SerializeObject(result));
+            }
+            else
+            {
+                return new HttpStatusCodeResult(404);
+            }
+        }
+    
+    }
+}
---
+++ SearchApp/Search/Searcher.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+
+namespace SearchApp.Search
+{
+    public class Searcher
+    {
+        public async System.Threading.Tasks.Task<JObject> SearchAsync(string name)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri("http://172.16.12.207:8082/api/appointments/");
+                HttpResponseMessage res = await client.GetAsync("http://172.16.12.207:8082/api/appointments/" + name);
+
+                if (res.IsSuccessStatusCode)
+                {
+                    string result = await res.Content.ReadAsStringAsync();
+                    JObject jsonresult = JObject.Parse(result);
+                    return jsonresult;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
